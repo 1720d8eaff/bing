53f9a11e5ae46772389e2f3fedd68dc16492294f
1720d8eaff <79614806+1720d8eaff@users.noreply.github.com>
2025-03-19 08:37:22 +0800

    添加了互动程序

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 import random
+
+
 width=21
 height=21
 maze = [[1 for _ in range(width)] for _ in range(height)]
+locationX=1
+locationY=0
 def dfs(x, y):
     maze[y][x] = 0  # 打通当前单元格
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -17,7 +21,34 @@ def dfs(x, y):
 dfs(1, 1)  # 从起点开始生成
 maze[1][0] = "起"  # 起点
 maze[width-1][height-2] = "终"
-for i in range(height):
-    for j in range(width):
-        print(maze[i][j], end=" ")
-    print()
+
+userinput = input()
+if userinput == "start":
+    print("ok")
+    for i in range(height):
+            for j in range(width):
+                print(maze[i][j], end=" ")
+            print()
+win=0
+while(win != 1):
+    Input=input()
+    if Input == "info":
+        print("ok",locationX, locationY, maze[locationX-1][locationY],maze[locationX+1][locationY],maze[locationX][locationY-1],maze[locationX][locationY+1])
+    if Input == "target":
+        print("ok",width-1,height-2)
+    if Input == "move 1":
+        locationX = locationX -1
+        print("ok")
+    if Input == "move 2":
+        locationX = locationX +1
+        print("ok")
+    if Input == "move 3":
+        locationY = locationY - 1
+        print("ok")
+    if Input == "move 4":
+        locationY = locationY +1
+        print("ok")
+    if locationX == width-1 and locationY == height-2:
+        win=1
+        print("success")
+    Input=" "
